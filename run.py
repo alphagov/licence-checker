8f37af002c6bb33acda9ba2fa598a9d243a597e9
David McDonald <idavidmcdonald@me.com>
2018-04-26 17:19:42 +0100

    Initial commit

---
+++ run.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import sys
+
+from github import Github
+from github.GithubException import UnknownObjectException
+
+
+def get_repos(client, org):
+	repos = client.get_organization(org).get_repos()
+	for repo in repos:
+		if not repo.archived:
+			yield repo
+
+
+def check_if_repo_has_license(client, repo):
+	repo.get_license()
+
+
+github_access_key = sys.argv[1]
+organisation = sys.argv[2]
+
+client = Github(github_access_key)
+
+repos_count = 0
+without_licence_count = 0
+for repo in get_repos(client, organisation):
+	repos_count += 1
+	try:
+		check_if_repo_has_license(client, repo)
+	except UnknownObjectException:
+		without_licence_count += 1
+		print("No licence found for {}".format(repo.name))
+
+print("found {} repos".format(repos_count))
+print("found {} repos without licences".format(without_licence_count))
