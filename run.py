146283ca65c10b1eb87e63162e31ba1cda676b6d
Isabell (Issy) Long <me@issyl0.co.uk>
2019-01-10 15:44:50 +0000

    My editor fixed the intentation of 'run.py' automatically

--- run.py
+++ run.py
@@ -6,14 +6,14 @@ from github.GithubException import UnknownObjectException
 
 
 def get_repos(client, org):
-	repos = client.get_organization(org).get_repos()
-	for repo in repos:
-		if not repo.archived:
-			yield repo
+    repos = client.get_organization(org).get_repos()
+    for repo in repos:
+        if not repo.archived:
+            yield repo
 
 
 def check_if_repo_has_license(client, repo):
-	repo.get_license()
+    repo.get_license()
 
 
 github_access_key = sys.argv[1]
@@ -24,12 +24,12 @@ client = Github(github_access_key)
 repos_count = 0
 without_licence_count = 0
 for repo in get_repos(client, organisation):
-	repos_count += 1
-	try:
-		check_if_repo_has_license(client, repo)
-	except UnknownObjectException:
-		without_licence_count += 1
-		print("No licence found for {}".format(repo.name))
+    repos_count += 1
+    try:
+        check_if_repo_has_license(client, repo)
+    except UnknownObjectException:
+        without_licence_count += 1
+        print("No licence found for {}".format(repo.name))
 
 print("found {} repos".format(repos_count))
 print("found {} repos without licences".format(without_licence_count))
